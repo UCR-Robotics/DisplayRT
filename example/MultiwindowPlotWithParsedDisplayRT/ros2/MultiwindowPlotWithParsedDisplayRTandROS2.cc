9c61348e5cfe623dccb2260377a3cc41b5f2f876
KeranYe <kyeaquarius@gmail.com>
2025-03-16 14:58:41 -0700

    tweaked on custom msg for ros2

--- example/MultiwindowPlotWithParsedDisplayRT/ros2/MultiwindowPlotWithParsedDisplayRTandROS2.cc
+++ example/MultiwindowPlotWithParsedDisplayRT/ros2/MultiwindowPlotWithParsedDisplayRTandROS2.cc
@@ -48,6 +48,7 @@
 
 using namespace display_rt; // for DisplayRT
 using namespace std::chrono_literals;
+using std::placeholders::_1;
 
 class MinimalPublisher : public rclcpp::Node
 {
@@ -73,10 +74,54 @@ class MinimalPublisher : public rclcpp::Node
     size_t count_;
 };
 
+class MinimalSubscriber : public rclcpp::Node
+{
+  public:
+    MinimalSubscriber()
+    : Node("minimal_subscriber")
+    {
+      subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const std_msgs::msg::String & msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
 int main(int argc, char *argv[])
 {
+    // rclcpp::init(argc, argv);
+    // rclcpp::spin(std::make_shared<MinimalPublisher>());
+    // rclcpp::shutdown();
+
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<MinimalPublisher>());
+
+    // ROS2 publisher thread
+    std::thread t0(
+        [&]{
+            // rclcpp::init(argc, argv);
+            rclcpp::spin(std::make_shared<MinimalPublisher>());
+            // rclcpp::shutdown();
+        }
+    );
+
+    // ROS2 subscriber thread
+    std::thread t1(
+        [&]{
+            // rclcpp::init(argc, argv);
+            rclcpp::spin(std::make_shared<MinimalSubscriber>());
+            // rclcpp::shutdown();
+        }
+    );
+
+    t0.join();
+    t1.join();
+
     rclcpp::shutdown();
+
     return 0;
 }
